ef4f48e5ec075579bffd73860be198bde3940812
captadexp <tech@developer.toys>
2025-04-06 03:14:07 +0530

    used rimraf instead of rm rf. some more minor cleanups

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -5,7 +5,7 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
     build: {
         lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
+            entry: resolve(__dirname, 'src/index.tsx'),
             name: 'NextBlogUI',
             fileName: 'index',
             formats: ['es'],
