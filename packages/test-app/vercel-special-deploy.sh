dd220ca9d89f9ad61c6131edf3db8aeca8e31d94
captadexp <tech@developer.toys>
2025-07-04 03:57:57 +0530

    minor fix with cp path in vercel deploy script

--- packages/test-app/vercel-special-deploy.sh
+++ packages/test-app/vercel-special-deploy.sh
@@ -2,5 +2,5 @@ NEXT_BLOG_DATA_PATH=/tmp
 bun --cwd=../.. run build
 bun --cwd=../.. i
 mkdir -p "public/api/next-blog/dashboard/static/"
-cp -r ../core/dist/assets/* ./public/api/next-blog/dashboard/static/
+cp -r ../core/dist/assets/@supergrowthai/next-blog-dashboard/static/* ./public/api/next-blog/dashboard/static/
 next build
