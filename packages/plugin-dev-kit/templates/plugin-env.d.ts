8fcef959297160f0a65c53a74dce695537746a4d
captadexp <tech@developer.toys>
2025-09-26 20:46:07 +0530

    added d.ts fiel

---
+++ packages/plugin-dev-kit/templates/plugin-env.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="@supergrowthai/plugin-dev-kit/client" />
+/// <reference types="@supergrowthai/plugin-dev-kit/server" />
+/// <reference types="@supergrowthai/jsx-runtime/jsx-runtime" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare module '@supergrowthai/plugin-dev-kit/client' {
+    export * from '@supergrowthai/plugin-dev-kit/client';
+}
+
+declare module '@supergrowthai/plugin-dev-kit/server' {
+    export * from '@supergrowthai/plugin-dev-kit/server';
+}
+
+declare module '@supergrowthai/jsx-runtime/jsx-runtime' {
+    export * from '@supergrowthai/jsx-runtime/jsx-runtime';
+}
