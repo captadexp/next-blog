e34cbf6f3ed0256fc2fc770596b156927b6703ff
captadexp <tech@developer.toys>
2025-10-14 14:38:44 +0530

    ci now publishes oneapi too. also made test-app robots and sitemap route force-dynamic

--- apps/test-app/app/robots.txt/route.ts
+++ apps/test-app/app/robots.txt/route.ts
@@ -1,5 +1,7 @@
 import {generateRobotsTxt} from "@supergrowthai/next-blog";
 import {dbProvider} from "@/lib/db";
 
+export const dynamic = 'force-dynamic';
+
 const {GET} = generateRobotsTxt(dbProvider);
 export {GET};
