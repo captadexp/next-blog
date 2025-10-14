fc088ee70973c38d55f34cb376f3ba8a496b12c5
captadexp <tech@developer.toys>
2025-10-14 15:50:31 +0530

    minor fix

--- apps/test-app/app/api/next-blog/[...page]/route.ts
+++ apps/test-app/app/api/next-blog/[...page]/route.ts
@@ -1,8 +1,11 @@
-import nextBlog from "@supergrowthai/next-blog";
+import {nextBlog} from "@supergrowthai/next-blog/next";
+import type {nextBlog as NextBlogType} from "@supergrowthai/next-blog";
 import {dbProvider} from "@/lib/db";
 
+const typedNextBlog = nextBlog as typeof NextBlogType;
+
 // Initialize Next-Blog
-const {GET, POST} = nextBlog({
+const {GET, POST} = typedNextBlog({
     db: dbProvider,
     ui: {
         branding: {
